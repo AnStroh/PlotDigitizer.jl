f1737f116fd2f294bb9726a210d104ba1de07a3e
Annalena Stroh <anstroh@uni-mainz.de>
2025-03-20 11:53:12 +0100

    Path

--- main codes/digitizePlot.jl
+++ main codes/digitizePlot.jl
@@ -1,6 +1,6 @@
 using PlotDigitizer
 
-file_name = "Examples_phase_diagram/Ol_Phase_diagram_without_framework.png"
+file_name = "../Examples_phase_diagram/Ol_Phase_diagram_without_framework.png"
 X_BC       = (0.0,   1.0)   #min max of X in the phase diagram
 Y_BC       = (0.0, 350.0)   #min max of Y in the phase diagram
 lines      = digitizePlot(X_BC, Y_BC, file_name)
